a2aac283809d2ce845e17d81449c154a9c390547
James Blaney <jamesblaney93@gmail.com>
2023-01-12 09:47:09 +0000

    basket total fixed

--- w8_hw/e-commerce-site/shoes-site/src/data/shoes.js
+++ w8_hw/e-commerce-site/shoes-site/src/data/shoes.js
@@ -2,37 +2,37 @@ const shoeData = [
     {
         brand: 'Nike ',
         name: 'Air Max 270',
-        cost: '£140',
+        cost: '140',
         size: '9'
     },
     {
         brand: 'Addidas ',
         name: 'Thing',
-        cost: '£140',
+        cost: '140',
         size: '9'
     },
     {
         brand: 'Air Jordan ',
         name: '11111',
-        cost: '£140',
+        cost: '140',
         size: '9'
     },
     {
         brand: 'Nike ',
         name: 'Air Max 270',
-        cost: '£140',
+        cost: '140',
         size: '9'
     },
     {
         brand: 'Nike ',
         name: 'Air Max 270',
-        cost: '£140',
+        cost: '140',
         size: '9'
     },
     {
         brand: 'Nike ',
         name: 'Air Max 270',
-        cost: '£140',
+        cost: '140',
         size: '9'
     }
 ];
