e700420be9284a4e7dd33310367feb091cc4a265
James Blaney <jamesblaney93@gmail.com>
2023-02-17 12:04:36 +0000

    music shop homework setup

---
+++ w13_hw/DrumsAreTheApexInstrument/src/main/java/stock/StockItem.java
@@ -0,0 +1,32 @@
+package stock;
+
+public class StockItem {
+
+    private double boughtPrice;
+    private double sellPrice;
+
+    public StockItem(double boughtPrice, double sellPrice) {
+        this.boughtPrice = boughtPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    public double getBoughtPrice() {
+        return boughtPrice;
+    }
+
+    public void setBoughtPrice(double boughtPrice) {
+        this.boughtPrice = boughtPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public double getMarkUp() {
+        return sellPrice - boughtPrice;
+    }
+}
---
+++ w13_hw/DrumsAreTheApexInstrument/src/test/java/stockItemTests/StockItemTest.java
@@ -0,0 +1,35 @@
+package stockItemTests;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import stock.StockItem;
+
+import static org.junit.Assert.assertEquals;
+
+public class StockItemTest {
+
+    private StockItem stockItem;
+    private double boughtPrice;
+    private double sellPrice;
+
+    @Before
+    public void setup() {
+            stockItem = new StockItem(5.99, 10.99);
+    }
+
+    @Test
+    public void hasBoughtPrice(){
+        assertEquals(5.99, stockItem.getBoughtPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellPrice(){
+        assertEquals(10.99, stockItem.getSellPrice(),0.01);
+    }
+
+    @Test
+    public void canReturnMarkUp(){
+        assertEquals(5.00, stockItem.getMarkUp(), 0.01);
+    }
+}
